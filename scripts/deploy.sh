1291adeefc723541fe4a18e21274d215e3513769
Sharath Babu <sharath.ise@gmail.com>
2024-02-12 12:33:52 +0530

    updated build file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ cd $WORKSPACE
 echo "Application will be deployed in 9000 port"
 
 source saswatfinenv/bin/activate
-python3 -m django runserver 0.0.0.0:9000
+python3 manage.py runserver 0.0.0.0:9000
