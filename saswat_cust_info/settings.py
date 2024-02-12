fd4a0c52c63239f0307719ef5630b1d5b5fea954
Sharath Babu <sharath.ise@gmail.com>
2024-02-12 12:13:47 +0530

    updated build file

--- saswat_cust_info/settings.py
+++ saswat_cust_info/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-5848@$j*jng$(%+-$734lezq#_n*td5+&5&-+#gc&xx)7^-f1_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [4.240.111.255]
 
 
 # Application definition
